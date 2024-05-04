3ecc52ffecf4ddef3b407ce04e4fa684114374e9
Ada BERK <adaberk00@gmail.com>
2024-05-04 18:07:33 +0300

    modern javascript/decoupled html

--- main.js
+++ main.js
@@ -1,34 +1,28 @@
-let kanal = 0;
-
-let yayinSec = () => {	
-	if (kanal >= 15) 
-		kanal = kanal % 15;
+let stream = 0;
+const changeStream = () => {	
+	if (stream >= 15) 
+		stream = stream % 15;
 	else
-		kanal += 1;
-	
-	return kanal;
+		stream += 1;
+
+	return stream;
 }
 
-let yayinDegistir = () => {
-	let img = document.getElementById("yayin");
-
-	img.src = "resimler/".concat(yayinSec().toString().concat(".jpg"));
+document.addEventListener('DOMContentLoaded', () => {
+	document.querySelector('#remote').addEventListener('click', () => {
+		let stream = document.querySelector("#stream");
 	
-	return false;
-}
-
-let degToRad = (x) => { return x * (Math.PI / 180); }
-let radToDeg = (x) => { return x * (180 / Math.PI); }
-
-let tan  = (x) => { return Math.tan(degToRad(x)); }
-let atan = (x) => { return (Math.atan(degToRad(x))); }
-let cot  = (x) => { return 1/tan(x); }
-
-let mesafeHesapla = () => {
-	let sonuc = document.getElementById("sonuc");
-	let aci   = parseInt(document.getElementById("deger").value);
+		stream.src = "img/".concat(changeStream().toString().concat(".jpg"));
+		
+		return false;
+	})
 	
-	sonuc.value =(635040000 - (aci ** 2))/(50400 * aci);
-	
-	return false;
-}
+	document.querySelector('#degrees').addEventListener('click', (ev) => {
+		const inputValue = parseInt(ev.target.value);
+		const outputBox  = document.getElementById("result");
+		
+		outputBox.value  = (635040000 - (aci ** 2)) / (50400 * inputValue);
+		
+		return false;
+	})
+})
